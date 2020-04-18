cd1f8da5b3c7ee180c2ca579b695bc689ce8a415
Paul Maynard <Paul.Maynard001@umb.edu>
2020-04-18 01:41:40 -0400

    typesafe heaps

--- Heap.hs
+++ Heap.hs
@@ -5,24 +5,24 @@ import Control.Applicative
 import Control.Monad.State
 import qualified Data.Map as M
 
-type Heap a = M.Map Handle a
-type Handle = Int
+type Heap a = M.Map (Handle a) a
+newtype Handle a = Handle {handleVal :: Int} deriving (Eq, Ord, Show, Read)
 
 emptyHeap :: Heap a
 emptyHeap = M.empty
 
-heapAlloc :: MonadState (Heap a) m => a -> m Handle
+heapAlloc :: MonadState (Heap a) m => a -> m (Handle a)
 heapAlloc a = do
-    h <- gets M.size
+    h <- gets $ Handle . M.size
     modify (M.insert h a)
     pure h
 
-heapPut :: (MonadState (Heap a) m, MonadPlus m) => Handle -> a -> m ()
+heapPut :: (MonadState (Heap a) m, MonadPlus m) => (Handle a) -> a -> m ()
 heapPut h a = gets (M.member h) >>= \case
     True  -> modify (M.insert h a)
     False -> empty
 
-heapGet :: (MonadState (Heap a) m, MonadPlus m) => Handle -> m a
+heapGet :: (MonadState (Heap a) m, MonadPlus m) => (Handle a) -> m a
 heapGet h = gets (M.lookup h) >>= \case
     Just a  -> pure a
     Nothing -> empty
